422fbe0d82e29a2bd16e55cd7359824ef1a7e836
rillakumaa <2200016119@webmail.uad.ac.id>
2024-07-02 22:10:20 +0700

    Initial commit

---
+++ tekmob_9/ios/Runner/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+  // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyAA6Bqg1ZiKEwutUZvcYLJn-pCAD6LM7I4")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
